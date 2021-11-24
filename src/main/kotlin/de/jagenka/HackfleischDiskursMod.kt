10123698a9d086ace2f7ee9774f3cca58643f3ca
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2021-11-24 14:39:52 +0100

    initial commit

---
+++ src/main/kotlin/de/jagenka/HackfleischDiskursMod.kt
@@ -0,0 +1,9 @@
+package de.jagenka
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object HackfleischDiskursMod: ModInitializer {
+    private const val MOD_ID = "hackfleisch-diskurs-mod"
+    override fun onInitialize() {
+        println("hackfleisch-diskurs-mod has been initialized.")
+    }
+}
