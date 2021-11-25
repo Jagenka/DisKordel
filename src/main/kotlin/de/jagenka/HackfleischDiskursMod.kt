b639d6de9f8e6ea339b6113327a134fd411a217f
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2021-11-25 11:31:48 +0100

    added jayCommand - pogress

--- src/main/kotlin/de/jagenka/HackfleischDiskursMod.kt
+++ src/main/kotlin/de/jagenka/HackfleischDiskursMod.kt
@@ -1,9 +1,24 @@
 package de.jagenka
+
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
+import net.minecraft.server.command.CommandManager
+
 @Suppress("UNUSED")
-object HackfleischDiskursMod: ModInitializer {
+object HackfleischDiskursMod : ModInitializer
+{
     private const val MOD_ID = "hackfleisch-diskurs-mod"
-    override fun onInitialize() {
+    override fun onInitialize()
+    {
         println("hackfleisch-diskurs-mod has been initialized.")
+
+        CommandRegistrationCallback.EVENT.register { dispatcher, dedicated ->
+            val jayCommand = CommandManager.literal("jay").executes {
+                println("jay!")
+                return@executes 0
+            }
+
+            dispatcher.register(jayCommand)
+        }
     }
 }
