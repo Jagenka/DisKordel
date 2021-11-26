1efb3501a80b1f5d51903f1cd59692832a95f9f4
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2021-11-26 11:15:08 +0100

    added mixins

---
+++ src/main/kotlin/de/jagenka/mixins/MinecraftServerMixin.java
@@ -0,0 +1,18 @@
+package de.jagenka.mixins;
+
+import de.jagenka.HackfleischDiskursMod;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerMixin
+{
+    @Inject(method = "loadWorld", at = @At("HEAD"))
+    private void serverLoaded(CallbackInfo ci)
+    {
+        HackfleischDiskursMod.onServerLoaded((MinecraftServer) (Object) this);
+    }
+}
