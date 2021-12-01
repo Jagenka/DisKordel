84edd6421215183598df9066ce64e1928837a0b4
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2021-12-01 01:00:28 +0100

    optimized imports

--- src/main/java/de/jagenka/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/de/jagenka/mixin/ServerPlayerEntityMixin.java
@@ -3,17 +3,12 @@ package de.jagenka.mixin;
 import de.jagenka.DiscordBot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.network.MessageType;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.UUID;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin
