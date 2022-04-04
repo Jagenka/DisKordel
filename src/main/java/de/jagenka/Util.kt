337cfb322e985c2a71c90cf305be61d0fc80737c
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2022-04-05 00:39:21 +0200

    added unwrap method to convert Java Optionals to Kotlin Nullable

--- src/main/java/de/jagenka/Util.kt
+++ src/main/java/de/jagenka/Util.kt
@@ -1,9 +1,13 @@
 package de.jagenka
 
+import java.util.*
+
 object Util
 {
     fun Double.trim(digits: Int): String
     {
         return "%.${digits}f".format(this)
     }
+    
+    fun <T> Optional<T>.unwrap(): T? = orElse(null)
 }
