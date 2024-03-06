d264ef560a171657cd94951b8b908ed8344c1931
Jagenka <dev@jayhannes.de>
2024-03-06 13:18:33 +0100

    implemented perf as slash command

--- src/main/kotlin/de/jagenka/commands/discord/PerfCommand.kt
+++ src/main/kotlin/de/jagenka/commands/discord/PerfCommand.kt
@@ -4,27 +4,47 @@ import com.mojang.brigadier.CommandDispatcher
 import de.jagenka.DiscordHandler
 import de.jagenka.MinecraftHandler
 import de.jagenka.Util.trimDecimals
+import de.jagenka.commands.DiskordelSlashCommand
 import de.jagenka.commands.DiskordelTextCommand
 import de.jagenka.commands.discord.MessageCommandSource.Companion.literal
 import de.jagenka.commands.discord.MessageCommandSource.Companion.redirect
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.RootInputChatBuilder
 
-object PerfCommand : DiskordelTextCommand
+object PerfCommand : DiskordelTextCommand, DiskordelSlashCommand
 {
+    override val name: String
+        get() = "performance"
+    override val description: String
+        get() = "Query current server ticks per second (TPS) and milliseconds per tick (MSPT)."
+
+    override suspend fun build(builder: RootInputChatBuilder)
+    {
+        // nothing needed
+    }
+
+    override suspend fun execute(event: ChatInputCommandInteractionCreateEvent)
+    {
+        event.interaction.respondEphemeral {
+            content = getResponse()
+        }
+    }
+
     override val internalId: String
         get() = "perf"
 
     override val shortHelpText: String
         get() = "query server performance"
     override val longHelpText: String
-        get() = "query the server's current ticks per second (TPS) and milliseconds per tick (MSPT)."
+        get() = "Query the server's current ticks per second (TPS) and milliseconds per tick (MSPT)."
 
     override fun registerWithDiscord(dispatcher: CommandDispatcher<MessageCommandSource>)
     {
         val commandNode =
             dispatcher.register(literal("perf")
                 .executes {
-                    val performanceMetrics = MinecraftHandler.getPerformanceMetrics()
-                    DiscordHandler.sendMessage("TPS: ${performanceMetrics.tps.toDouble().trimDecimals(1)} MSPT: ${performanceMetrics.mspt.trimDecimals(1)}", silent = true)
+                    DiscordHandler.sendMessage(getResponse(), silent = true)
                     0
                 })
 
@@ -32,5 +52,11 @@ object PerfCommand : DiskordelTextCommand
 
         Registry.registerShortHelpText(shortHelpText, commandNode, aliasCommandNode)
         Registry.registerLongHelpText(longHelpText, commandNode, aliasCommandNode)
+    }
+
+    private fun getResponse(): String
+    {
+        val performanceMetrics = MinecraftHandler.getPerformanceMetrics()
+        return "TPS: ${performanceMetrics.tps.toDouble().trimDecimals(1)} MSPT: ${performanceMetrics.mspt.trimDecimals(1)}"
     }
 }
