1063cd2a2aa37191178e067e0ad701d9fcace282
Jagenka <dev@jayhannes.de>
2024-03-06 13:26:38 +0100

    implement list command as slash command

--- src/main/kotlin/de/jagenka/commands/discord/ListCommand.kt
+++ src/main/kotlin/de/jagenka/commands/discord/ListCommand.kt
@@ -3,13 +3,34 @@ package de.jagenka.commands.discord
 import com.mojang.brigadier.CommandDispatcher
 import de.jagenka.DiscordHandler
 import de.jagenka.MinecraftHandler
+import de.jagenka.commands.DiskordelSlashCommand
 import de.jagenka.commands.DiskordelTextCommand
 import de.jagenka.commands.discord.MessageCommandSource.Companion.literal
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.RootInputChatBuilder
 
-object ListCommand : DiskordelTextCommand
+object ListCommand : DiskordelTextCommand, DiskordelSlashCommand
 {
     override val internalId: String
         get() = "list"
+
+    override val name: String
+        get() = "list"
+    override val description: String
+        get() = "List players, who are currently connected to the Minecraft Server."
+
+    override suspend fun build(builder: RootInputChatBuilder)
+    {
+        // nothing needed
+    }
+
+    override suspend fun execute(event: ChatInputCommandInteractionCreateEvent)
+    {
+        event.interaction.respondEphemeral {
+            content = getResponse()
+        }
+    }
 
     override val shortHelpText: String
         get() = "list online players"
@@ -21,21 +42,25 @@ object ListCommand : DiskordelTextCommand
         val commandNode = dispatcher.register(
             literal("list")
                 .executes {
-                    val onlinePlayers = MinecraftHandler.getOnlinePlayers()
-                    DiscordHandler.sendMessage(
-                        text = "Currently online:\n" +
-                                if (onlinePlayers.isEmpty()) "~nobody~"
-                                else
-                                {
-                                    onlinePlayers.joinToString(separator = "\n") {
-                                        "- $it"
-                                    }
-                                }, silent = true)
+                    DiscordHandler.sendMessage(text = getResponse(), silent = true)
                     0
                 }
         )
 
         Registry.registerShortHelpText(shortHelpText, commandNode)
         Registry.registerLongHelpText(longHelpText, commandNode)
+    }
+
+    private fun getResponse(): String
+    {
+        val onlinePlayers = MinecraftHandler.getOnlinePlayers()
+        return "Currently online:\n" +
+                if (onlinePlayers.isEmpty()) "~nobody~"
+                else
+                {
+                    onlinePlayers.joinToString(separator = "\n") {
+                        "- $it"
+                    }
+                }
     }
 }
