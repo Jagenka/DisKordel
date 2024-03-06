e8ce3aa12597e97289d5b1a27957c7a160f00965
Jagenka <dev@jayhannes.de>
2024-03-06 09:15:06 +0100

    rewrite of Minecraft to Discord relay should work better now (?)

--- src/main/java/de/jagenka/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/de/jagenka/mixin/PlayerAdvancementTrackerMixin.java
@@ -1,10 +1,24 @@
 package de.jagenka.mixin;
 
+import de.jagenka.MinecraftHandler;
+import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.PlayerAdvancementTracker;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerAdvancementTracker.class)
-public class PlayerAdvancementTrackerMixin
+public abstract class PlayerAdvancementTrackerMixin
 {
+    @Shadow
+    private ServerPlayerEntity owner;
 
+    @Inject(method = "grantCriterion", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V"))
+    private void onAdvancementGet(AdvancementEntry advancement, String criterionName, CallbackInfoReturnable<Boolean> cir)
+    {
+        MinecraftHandler.handleAdvancementGet(advancement, this.owner);
+    }
 }
