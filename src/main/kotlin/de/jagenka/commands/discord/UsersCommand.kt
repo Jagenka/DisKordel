d20966f7acead2f20b90cc24cdf265110acd8b48
Jagenka <dev@jayhannes.de>
2024-03-06 13:59:45 +0100

    implement users command as slash command

--- src/main/kotlin/de/jagenka/commands/discord/UsersCommand.kt
+++ src/main/kotlin/de/jagenka/commands/discord/UsersCommand.kt
@@ -2,16 +2,38 @@ package de.jagenka.commands.discord
 
 import com.mojang.brigadier.CommandDispatcher
 import de.jagenka.DiscordHandler
+import de.jagenka.DiscordHandler.asCodeBlock
 import de.jagenka.Main
 import de.jagenka.UserRegistry
+import de.jagenka.commands.DiskordelSlashCommand
 import de.jagenka.commands.DiskordelTextCommand
 import de.jagenka.commands.discord.MessageCommandSource.Companion.literal
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.RootInputChatBuilder
 import kotlinx.coroutines.launch
 
-object UsersCommand : DiskordelTextCommand
+object UsersCommand : DiskordelTextCommand, DiskordelSlashCommand
 {
     override val internalId: String
         get() = "users"
+
+    override val name: String
+        get() = "users"
+    override val description: String
+        get() = "List all registered users."
+
+    override suspend fun build(builder: RootInputChatBuilder)
+    {
+        // nothing to declare
+    }
+
+    override suspend fun execute(event: ChatInputCommandInteractionCreateEvent)
+    {
+        event.interaction.respondEphemeral {
+            content = UserRegistry.getAllUsersAsOutput().asCodeBlock()
+        }
+    }
 
     override val shortHelpText: String
         get() = "list all registered users"
